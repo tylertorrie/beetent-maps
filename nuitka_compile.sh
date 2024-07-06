e4f875ad343b430ff418b3934d87de054c1d1877
Michael Torrie <torriem@gmail.com>
2024-07-05 20:58:48 -0600

    rudimentary overwrite check added to nuitka_compile.sh

--- nuitka_compile.sh
+++ nuitka_compile.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ -d "beetent_trimble" ]; then
+	echo "Output dir beetent_trimble exists. Please remove it before"
+	echo "building a new one."
+	exit 1
+fi
+
 python -m nuitka  maketentgrid.py --enable-plugin=tk-inter -o maketentgrid.exe --follow-imports  --standalone
 
 if [ "$?" -eq 0 ]; then
 	mv maketentgrid.dist beetent_trimble
 fi
 
+
+echo Moved maketentgrid.dist to beetent_trimble.  Zip that up to distribute.
